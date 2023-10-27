0a9978aebe66cfe0a413e6406bcb910255bd1357
Piyush-onGIT <thisissahupiyuh@gmail.com>
2023-10-27 14:32:14 +0530

    admin panel collection typo

--- controllers/AdminControllers.js
+++ controllers/AdminControllers.js
@@ -1,7 +1,7 @@
 const fetchAll = async (req, res) => {
   const db = req.db;
   const event = req.query.event;
-  const collection = event === "BGMI" ? event + "_Registration" : event + "_registration";
+  const collection = (event === "BGMI" || event === "MechanicalJunkyard" || event === "Hydrolift") ? event + "_Registration" : event + "_registration";
   let data = await db.collection(collection).find({})
   data = await data.toArray();
   return res.status(200).json({ ok: true, message: data })
