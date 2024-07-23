317d6ac7fb85ede8ba1df0fb7f1f3ecf91b04612
Piyush-onGIT <piyush.at.googl@gmail.com>
2024-07-23 20:38:56 +0530

    removed rate limited

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response, NextFunction } from "express";
-import rateLimit from "express-rate-limit";
+// import rateLimit from "express-rate-limit";
 import cors from "cors";
 import bodyParser from "body-parser";
 import connectToDatabase from "./db/conn";
